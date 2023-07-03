2d715eea1fbff1a35cb56926ad0aa06b3bda9f0b
ThiagoMonts <thiagohonorato@hotmail.com>
2023-07-03 16:55:57 -0300

    JWT encode and decode token

--- lib/session.ts
+++ lib/session.ts
@@ -15,14 +15,22 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-//    jwt: {
-//        encode: ({ secret, token }) => {
-//
-//        },
-//        decode: async ({ secret, token }) => {
-//            
-//        }
-//    },
+    jwt: {
+        encode: ({ secret, token }) => {
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss: 'grafbase',
+                exp: Math.floor(Date.now() / 1000) + 60 * 60
+            }, secret)
+
+            return encodedToken;
+        },
+        decode: async ({ secret, token }) => {
+            const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+
+            return decodedToken;
+        }
+    },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
@@ -51,14 +59,14 @@ export const authOptions: NextAuthOptions = {
         },
         async signIn({ user }: { user: AdapterUser | User }) {
             try {
-                const userExists = await getUser(user?.email as string) as { user?: UserProfile};
+                const userExists = await getUser(user?.email as string) as { user?: UserProfile };
 
                 if (!userExists.user) {
                     await createUser(
-                        user.name as string, 
-                        user.email as string, 
+                        user.name as string,
+                        user.email as string,
                         user.image as string
-                        )
+                    )
                 }
 
                 return true;
