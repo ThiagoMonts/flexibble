8c89bf2f8c4ac15f99d495283ee5e3dcc46e8702
ThiagoMonts <thiagohonorato@hotmail.com>
2023-07-06 18:34:34 -0300

    Criação do FormField

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -3,6 +3,8 @@
 import { SessionInterface } from "@/common.types"
 import Image from "next/image"
 import { ChangeEvent } from "react"
+import FormField from "./FormField"
+import { categoryFilters } from "@/constants"
 
 type Props = {
   type: string
@@ -55,6 +57,43 @@ const ProjectForm = ({ type, session }: Props) => {
         placeholder="Flexible"
         setState={(value) => handleStateChange('title', value)}
       />
+      <FormField 
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value) => handleStateChange('description', value)}
+      />
+      <FormField
+        type="url" 
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://thiagohonorato.netlify.app/"
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+      <FormField
+        type="url" 
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/ThiagoMonts"
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+      <FormField 
+        title="Title"
+        state={form.title}
+        placeholder="Flexible"
+        setState={(value) => handleStateChange('title', value)}
+      />
+
+      <CustomMenu 
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
+      />
+
+      <div className="flexStart w-full">
+          <button>Create</button>
+      </div>
     </form>
   )
 }
