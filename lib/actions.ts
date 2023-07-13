600ae29f99877a60307d8e79eda035a3a39e62f7
ThiagoMonts <thiagohonorato@hotmail.com>
2023-07-13 12:03:47 -0300

    Correção de bug

--- lib/actions.ts
+++ lib/actions.ts
@@ -44,34 +44,34 @@ export const fetchToken = async () => {
 
 export const uploadImage = async (imagePath: string) => {
     try {
-        const response = await fetch(`${serverUrl}/api/upload`, {
-            method: "POST",
-            body: JSON.stringify({ 
-                path: imagePath 
-            }),
-        });
-        return response.json();
+      const response = await fetch(`${serverUrl}/api/upload`, {
+        method: "POST",
+        body: JSON.stringify({
+          path: imagePath,
+        }),
+      });
+      return response.json();
     } catch (err) {
-        throw err;
+      throw err;
     }
-};
+  };
 
 export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
     const imageUrl = await uploadImage(form.image);
-
+  
     if (imageUrl.url) {
-        client.setHeader("Authorization", `Bearer ${token}`);
-
-        const variables = {
-            input: {
-                ...form,
-                image: imageUrl.url,
-                createdBy: {
-                    link: creatorId
-                }
-            }
+      client.setHeader("Authorization", `Bearer ${token}`);
+  
+      const variables = {
+        input: { 
+          ...form, 
+          image: imageUrl.url, 
+          createdBy: { 
+            link: creatorId 
+          }
         }
-
-        return makeGraphQLRequest(createProjectMutation, variables)
+      };
+  
+      return makeGraphQLRequest(createProjectMutation, variables);
     }
-}
+  };
