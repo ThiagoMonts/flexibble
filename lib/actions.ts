9eb1282edf01a4b73437b79223a6071dd8f7e1c2
ThiagoMonts <thiagohonorato@hotmail.com>
2023-07-13 11:24:11 -0300

    Correção de bug

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,7 +5,8 @@ import { GraphQLClient } from "graphql-request";
 const isProduction = process.env.NODE_ENV === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
-const serveUrl = isProduction ? process.env.NEXT_PUBLIC_SERVE_URL || '' : 'http://localhost:3000'
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
+
 
 const client = new GraphQLClient(apiUrl)
 
@@ -34,7 +35,7 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
 
 export const fetchToken = async () => {
     try {
-        const response = await fetch(`${serveUrl}/api/auth/token`);
+        const response = await fetch(`${serverUrl}/api/auth/token`);
         return response.json();
     } catch (error) {
         throw error;
@@ -43,16 +44,17 @@ export const fetchToken = async () => {
 
 export const uploadImage = async (imagePath: string) => {
     try {
-        const response = await fetch(`${serveUrl}/api/upload`, {
-            method: 'POST',
-            body: JSON.stringify({ path: imagePath })
-        })
-
+        const response = await fetch(`${serverUrl}/api/upload`, {
+            method: "POST",
+            body: JSON.stringify({ 
+                path: imagePath 
+            }),
+        });
         return response.json();
-    } catch (error) {
-        throw error;
+    } catch (err) {
+        throw err;
     }
-}
+};
 
 export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
     const imageUrl = await uploadImage(form.image);
