9c85010922e0fcde7c15e7e40ff9fb248001e9fd
ThiagoMonts <thiagohonorato@hotmail.com>
2023-07-20 18:06:18 -0300

    Implementação da Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions"
 
@@ -17,16 +18,21 @@ type ProjectSearch = {
 
 type SearchParams = {
     category?: string;
+    endcursor?: string;
 }
 
 type Props = {
     searchParams: SearchParams;
 }
 
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+export const revalidate = 0
 
-const Home = async ({ searchParams: { category }}: Props) => {
 
-    const data = await fetchAllProjects(category) as ProjectSearch;
+const Home = async ({ searchParams: { category, endcursor }}: Props) => {
+
+    const data = await fetchAllProjects(category, endcursor) as ProjectSearch;
 
     const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -39,6 +45,8 @@ const Home = async ({ searchParams: { category }}: Props) => {
             </section>
         )
     }
+
+    const pagination = data?.projectSearch?.pageInfo;
 
     return (
         <section className="flex-start flex-col paddings mb-16">
@@ -56,7 +64,12 @@ const Home = async ({ searchParams: { category }}: Props) => {
                     />
                 ))}
             </section>
-            <h1>LoadMore</h1>
+            <LoadMore
+                startCursor={pagination.startCursor}
+                endCursor={pagination.endCursor}
+                hasPreviousPage={pagination.hasPreviousPage}
+                hasNextPage={pagination.hasNextPage}
+            />
         </section>
     )
 }
